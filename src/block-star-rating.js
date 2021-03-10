da8b2120ae0fc267104410408db1541248d5e059
codermartz <codermartz@gmail.com>
2021-03-10 10:56:08 +0800

    Making text-domain compatible with external parsers

--- src/block-star-rating.js
+++ src/block-star-rating.js
@@ -6,28 +6,28 @@ const { InspectorControls, useBlockProps } = wp.blockEditor;
 const { PanelBody, RangeControl } = wp.components;
 
 registerBlockType( 'devton/star-rating', {
-	title: __( 'Star Rating', 'devton' ),
-	description: __( 'Rate content based on importance and usefulness', 'devton' ),
+	title: __( 'Star Rating', 'star-rating' ),
+	description: __( 'Rate content based on importance and usefulness', 'star-rating' ),
 	category: 'widgets',
 	icon: 'star-empty',
 	keywords: [ 
-		__( 'Star', 'devton' ),
-		__( 'Rating', 'devton' ),
-		__( 'Rate', 'devton' ),
+		__( 'Star', 'star-rating' ),
+		__( 'Rating', 'star-rating' ),
+		__( 'Rate', 'star-rating' ),
 	],
 	styles: [
 		{
 			name: 'full',
-			label: __( 'Full', 'devton' ),
+			label: __( 'Full', 'star-rating' ),
 			isDefault: true,
 		},
 		{
 			name: 'outline',
-			label: __( 'Outline', 'devton' ),
+			label: __( 'Outline', 'star-rating' ),
 		},
 		{
 			name: 'colored',
-			label: __( 'Colored', 'devton' ),
+			label: __( 'Colored', 'star-rating' ),
 		}
 	],
 	attributes: {
@@ -45,11 +45,11 @@ registerBlockType( 'devton/star-rating', {
 			<div className={ className }>
 				<InspectorControls>
 					<PanelBody
-						title={ __( 'Star Rating Settings', 'devton' ) }
+						title={ __( 'Star Rating Settings', 'star-rating' ) }
 						initialOpen={ true }
 					>
 						<RangeControl
-							label={ __( 'Number of stars', 'devton' ) }
+							label={ __( 'Number of stars', 'star-rating' ) }
 							value={ attributes.count }
 							onChange={ newValue => setAttributes( { count: newValue } ) }
 							min={1}
